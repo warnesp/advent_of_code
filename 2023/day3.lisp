c3539426433520b17f4b1e9a6165e3c67c7b83af
Paul Warnes <pwarnes@gmail.com>
2023-12-04 20:40:48 -0600

    day 3 partial, day 4 complete

--- 2023/day3.lisp
+++ 2023/day3.lisp
@@ -2,25 +2,98 @@
 
 (load  "utilities.lisp")
 
-(defun read-calibration (line)
-  (remove nil (mapcar #'digit-char-p (coerce line 'list))))
+(defun get-symbol (c)
+  (when (not (or (eq c #\.) (digit-char-p c)))
+    c))
 
+(defun read-schematic (line)
+  (let ((new-num 0))
+    (loop for x from 0 below (length line)
+          for potential-num = (parse-integer (subseq line x) :junk-allowed t)
+          when (not (digit-char-p (char line x)))
+            do (setf new-num 0)
+          when (and (eq new-num 0) potential-num (digit-char-p (char line x)))
+            do (setf new-num potential-num)
+          collect (cons (get-symbol (char line x)) new-num) )))
+(defun sum-same-line-parts (line)
+  (+
+   (loop for prev in line
+         for cur in (cdr line) 
+         when (car cur)
+           sum (cdr prev))
+   (loop for cur in line 
+        for next in (cdr line) 
+        when (car cur)
+          sum  (cdr next)
+        )))
+(defun sum-other (left center right)
+  (cond ((= left center right)  left)
+        ((and (= left center) (zerop right)) left)
+        ((and (zerop left) (= center right)) right)
+        (t (+ left center right))))
+
+;; TODO add in corner cases where symbol is at start or end of line
+(defun sum-other-line-parts (line other)
+  (fresh-line)
+  (princ "===>")
+  (princ (caar (last line)))
+  (princ "===>")
+  (princ (cdar (last other 2)))
+  (fresh-line)
+  (+ (loop for prev in other
+           for cur in (cdr other) 
+           for next in (cddr other) 
+           for pos in (cdr line) 
+           when (car pos)
+             do (progn (princ (cdr prev))  (princ " ") (princ (cdr cur)) (princ " ") (princ (cdr next)) (princ " = ") (princ (sum-other (cdr prev) (cdr cur) (cdr next))) (fresh-line)) 
+           when (car pos)
+             sum (sum-other (cdr prev) (cdr cur) (cdr next))
+           )
+     (if (caar line) (sum-other 0 (cdar other) (cdadr other) ) 0)
+     (if (caar (last line)) (sum-other  (cdar (last other 2)) (cdar (last other)) 0) 0)
+     ))
+
+
+(defun sum-parts (lines)
+  (+ (loop for line in lines
+           sum (sum-same-line-parts line))
+     (loop for prev in lines
+           for cur in (cdr lines)
+           sum (sum-other-line-parts cur prev))
+     (loop for cur in lines
+           for next in (cdr lines)
+           sum (sum-other-line-parts cur next))
+     )
+  )
+
+;; 4361
 (defun day3-test1 ()
-   (sum-cals (read-file "inputs/day2-test" #'read-calibration)))
-
+   (sum-parts (read-file "inputs/day3-test1" #'read-schematic)))
+;; 413
+(defun day3-test2 ()
+  (sum-parts (read-file "inputs/day3-test2" #'read-schematic)))
+;; 530751 too low
+;; 533767 too low
+;; 533775
 (defun day3-real1 ()
-  (sum-cals (read-file "inputs/day2" #'read-calibration)))
+  (sum-parts (read-file "inputs/day3" #'read-schematic)))
 
 
 ;;;;;;;;;;;
 ;; Part 2
 ;;;;;;;;;;;
 
+(defun sum-gears (lines)
+  )
 
-
-
-(defun day3-test2 ()
-   (sum-cals (read-file "inputs/day2-test2" #'read-calibration-alt)))
-
+;;467835
+(defun day3-test3 ()
+  (sum-gears (read-file "inputs/day3-test1" #'read-schematic)))
+;;6756
+(defun day3-test4 ()
+  (sum-gears (read-file "inputs/day3-test2" #'read-schematic)))
 (defun day3-real2 ()
-  (sum-cals (read-file "inputs/day2" #'read-calibration-alt)))
+  (sum-gears (read-file "inputs/day3" #'read-schematic)))
+
+
+
--- 2023/day4.lisp
+++ 2023/day4.lisp
@@ -1,26 +1,74 @@
 ;; 2023 Advent of Code Day 4
 
 (load  "utilities.lisp")
+(ql:quickload "cl-ppcre")
 
-(defun read-calibration (line)
-  (remove nil (mapcar #'digit-char-p (coerce line 'list))))
+(defun create-card (num winning have)
+  (list num winning have))
 
+(defun parse-nums (nums)
+  (loop for x from 0 below (length nums) by 3
+        collect (parse-integer (subseq nums x) :junk-allowed t)))
+
+(defun read-scratchcard (line)
+  (let* ((s1 (cl-ppcre:split ":" line))
+         (s2 (cl-ppcre:split #\| (cadr s1))))
+  (create-card (parse-integer (subseq (car s1) 5) :junk-allowed t)
+               (parse-nums (subseq (car s2) 1))
+               (parse-nums (subseq (cadr s2) 1)))
+    ))
+(defun count-winners (card)
+  (let ((winners (sort (cadr card) #'<))
+        (have (sort (caddr card) #'<)))
+    (loop for w in winners
+          when (find w have)
+            sum 1)
+    ))
+
+(defun calc-points (card)
+  (let ((winners (count-winners card)))
+    (if (= 0 winners) 0
+        (expt 2 (- winners 1))
+    )))
+
+(defun sum-points (cards)
+  (apply #'+ (mapcar #'calc-points cards)))
+;; 13
 (defun day4-test1 ()
-   (sum-cals (read-file "inputs/day2-test" #'read-calibration)))
+  (sum-points (read-file "inputs/day4-test1" #'read-scratchcard)))
 
+;;27059
 (defun day4-real1 ()
-  (sum-cals (read-file "inputs/day2" #'read-calibration)))
+  (sum-points (read-file "inputs/day4" #'read-scratchcard)))
 
 
 ;;;;;;;;;;;
 ;; Part 2
 ;;;;;;;;;;;
 
+(defun update-dupes (dupes winners mult)
+  (let ((new-dupes (append
+                    (loop for d in dupes
+                          repeat winners
+                          collect (+ d mult))
+                    (subseq dupes (min (length dupes) winners))
+                    (loop repeat (- winners (length dupes))
+                          collect mult))))
+    (if new-dupes new-dupes '(0))))
 
-
-
+(defun count-cards (cards dupes total)
+  (if cards
+      (let* ((winners (count-winners (car cards)))
+             (mult (1+ (car dupes)))
+             (new-dupes (update-dupes (cdr dupes) winners mult))
+             )
+        (count-cards (cdr cards) new-dupes (+ total mult))
+        
+        )
+      total))
+;; 30
 (defun day4-test2 ()
-   (sum-cals (read-file "inputs/day2-test2" #'read-calibration-alt)))
-
+   (count-cards (read-file "inputs/day4-test1" #'read-scratchcard) '(0) 0))
+;; 5744979
 (defun day4-real2 ()
-  (sum-cals (read-file "inputs/day2" #'read-calibration-alt)))
+  (count-cards (read-file "inputs/day4" #'read-scratchcard) '(0) 0))
