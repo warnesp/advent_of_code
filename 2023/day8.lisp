bd915db5bd612ca8527ba36a464c78795f659bff
Paul Warnes <pwarnes@gmail.com>
2023-12-09 21:47:07 -0600

    Day 9 complete

---
+++ 2023/day8.lisp
@@ -0,0 +1,118 @@
+;; 2023 Advent of Code Day 8
+
+(load  "utilities.lisp")
+(ql:quickload "cl-ppcre")
+(ql:quickload "str")
+
+(defstruct node start left right is-end is-start)
+
+(defstruct network path nodes starts ends)
+
+(defun nodes-to-hash (nodes)
+  (let ((tbl (make-hash-table)))
+    (loop for node in nodes
+          do (setf (gethash (node-start node) tbl) node))
+    tbl))
+
+(defun data-to-network (lines)
+  (make-network :path (car lines)
+                :nodes (nodes-to-hash (cdr lines))
+                :starts (mapcar #'node-start (remove-if-not #'node-is-start (cdr lines)))
+                :ends (mapcar #'node-start (remove-if-not #'node-is-end (cdr lines)))))
+
+(defun parse-path (line)
+  (mapcar  (lambda (c) (if (eq c #\L) 'L 'R)) (coerce line 'list)))
+
+(defun parse-node (line)
+  (let* ((parts (str:split-omit-nulls #\= line))
+         (is-start (str:ends-with? "A" (str:trim (car parts))))
+         (is-end (str:ends-with? "Z" (str:trim (car parts))))
+         (paths (read-from-string (str:trim (str:replace-all "," #\Space (cadr parts))))))
+    (make-node :start (intern (str:trim (car parts)))
+               :left (car paths)
+               :right (cadr paths)
+               :is-start is-start
+               :is-end is-end)
+    ))
+
+(defun read-network (line)
+  (cond
+    ((find #\= line) (parse-node line))
+    ((= 0 (length line)) nil)
+    (t (parse-path line))
+    ))
+
+(defun get-next (node instr)
+  (if (eq 'L instr)
+      (node-left node)
+      (node-right node)))
+
+(defun to-steps (network)
+  (labels ((pts-rec (network instr current cnt)
+             (if (eq 'ZZZ current)
+                 cnt
+                 (pts-rec network (or (cdr instr) (network-path network)) (get-next (gethash current (network-nodes network)) (car instr)) (1+ cnt))
+                 )))
+    (pts-rec network (network-path network) 'AAA 0)
+    ))
+
+;;;;;;;;;;;;
+;; Part 1 ;;
+;;;;;;;;;;;;
+
+;; 2
+(defun day8-test1 ()
+  (to-steps (data-to-network (remove nil (read-file "inputs/day8-test1" #'read-network)))) 
+  )
+
+;; 6
+(defun day8-test2 ()
+  (to-steps (data-to-network (remove nil (read-file "inputs/day8-test2" #'read-network)))) 
+  )
+
+
+;; 11567
+(defun day8-real1 ()
+  (to-steps (data-to-network (remove nil (read-file "inputs/day8" #'read-network)))) 
+  )
+
+
+;;;;;;;;;;;;
+;; Part 2 ;;
+;;;;;;;;;;;;
+
+(defun is-at-end (ends currents)
+  (not (member nil (loop for c in currents
+        collect (member c ends)))))
+
+(defun follow-paths (network currents instr)
+  (loop for current in currents
+        collect (get-next (gethash current (network-nodes network)) instr))
+  
+  )
+
+(defun to-steps2 (network)
+  (labels ((pts-rec (instr current cnt)
+             (if (node-is-end current)
+                 cnt
+                 (pts-rec (or (cdr instr) (network-path network))
+                          (gethash (get-next current (car instr)) (network-nodes network))
+                          (1+ cnt))
+                 )))
+    (loop for start in (network-starts network)
+          collect (pts-rec (network-path network) (gethash start (network-nodes network)) 0))
+    
+    ))
+
+
+;; 6
+(defun day8-test3 ()
+  (apply #'lcm (to-steps2 (data-to-network (remove nil (read-file "inputs/day8-test3" #'read-network)))))
+  )
+
+
+;; 9858474970153
+(defun day8-real2 ()
+  (apply #'lcm (to-steps2 (data-to-network (remove nil (read-file "inputs/day8" #'read-network)))))
+  )
+
---
+++ 2023/day9.lisp
@@ -0,0 +1,74 @@
+;; 2023 Advent of Code Day 9
+
+(load  "utilities.lisp")
+(ql:quickload "cl-ppcre")
+(ql:quickload "str")
+
+
+(defun expand-seq (seq)
+  (if (= (length (car seq)) (count 0  (car seq) ))
+      seq
+      (expand-seq (cons (loop for i in (car seq)
+                              for j in (cdar seq)
+                              collect (- j i)) seq))))
+
+
+(defun predict-next (seq)
+  (labels ((pn-rec (seq accum)
+                   (if seq
+                       (pn-rec (cdr seq) (cons (+ (car (last (car seq)))  (car accum)) accum))
+                       accum)))
+          (car (pn-rec seq '(0)))
+          ))
+
+(defun read-report (line)
+  (list (mapcar #'parse-integer (str:split #\Space line))))
+
+
+;;;;;;;;;;;;
+;; Part 1 ;;
+;;;;;;;;;;;;
+
+;; 114
+(defun day9-test1 ()
+  (apply #'+ (mapcar #'predict-next (mapcar #'expand-seq (read-file "inputs/day9-test1" #'read-report))))
+  )
+
+
+;; 1987402313
+(defun day9-real1 ()
+  (apply #'+ (mapcar #'predict-next (mapcar #'expand-seq (read-file "inputs/day9" #'read-report))))
+  )
+
+
+;;;;;;;;;;;;
+;; Part 2 ;;
+;;;;;;;;;;;;
+
+(defun read-report-r (line)
+  (list (reverse (mapcar #'parse-integer (str:split #\Space line)))))
+
+
+;; 
+(defun day9-test2 ()
+  (apply #'+ (mapcar #'predict-next (mapcar #'expand-seq (read-file "inputs/day9-test1" #'read-report-r))))
+  )
+
+
+;; 1987402313
+(defun day9-real2 ()
+  (apply #'+ (mapcar #'predict-next (mapcar #'expand-seq (read-file "inputs/day9" #'read-report-r))))
+  )
+
+
+;; 
+(defun day9-test2 ()
+  (apply #'lcm (to-steps3 (data-to-network (remove nil (read-file "inputs/day9-test3" #'read-network)))))
+  )
+
+
+;; 
+(defun day9-real2 ()
+  (apply #'lcm (to-steps2 (data-to-network (remove nil (read-file "inputs/day9" #'read-network)))))
+  )
+
